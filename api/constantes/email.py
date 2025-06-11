cb1ed2b6cbf766f13206d93f3f69f727b79e20c1
angelLopezITECAM <angel.lopez@itecam.com>
2025-06-11 11:56:41 +0200

    Dejar configurado el gmail de hidrogeno

--- api/constantes/email.py
+++ api/constantes/email.py
@@ -6,13 +6,14 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 
-SERVIDOR = "smtp.office365.com"
+""" SERVIDOR = "smtp.office365.com"
 PUERTO_SMTP = 587
 REMITENTE = "diseno@itecam.com"
-PASSWORD = "It_Disen0"
+PASSWORD = "It_Disen0" """
 
-""" SERVIDOR = "smtp-relay.gmail.com"
+SERVIDOR = "smtp.gmail.com"
 PUERTO_SMTP = 587
 REMITENTE = "celdaemh2@gmail.com"
-PASSWORD = "AEM_H2.2025"
- """
+PASSWORD = "tbqn ijuv kisg mycy"
+""" REMITENTE = "padelangeltomelloso@gmail.com"
+PASSWORD = "kyby cbxd nlgi xxem" """
